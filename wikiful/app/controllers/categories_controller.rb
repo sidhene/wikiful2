394c5c29f4a99663c4795beecc87e30159e3d9b9
Amy Lightholder <sidhene@gmail.com>
2014-10-30 23:17:57 -0700

    fixed controllers again

--- wikiful/app/controllers/categories_controller.rb
+++ wikiful/app/controllers/categories_controller.rb
@@ -2,4 +2,9 @@ class CategoriesController < ApplicationController
   def index
   	@categories = Category.all.order(name: :asc)
   end
+  
+  def show
+  	@category = Category.find(params[:id])
+  end
+
 end
