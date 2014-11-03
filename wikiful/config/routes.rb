7c57d4b3c26fdd4bd6ec77fd998a33418d89ddee
Amy Lightholder <sidhene@gmail.com>
2014-11-03 13:21:26 -0800

    finished users feature

--- wikiful/config/routes.rb
+++ wikiful/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+get 'signup', to: 'users#new', as: 'signup'
+get 'login', to: 'sessions#new', as: 'login'
+get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :sessions
   resources :users
 
   get 'welcome/index'
